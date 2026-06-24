2c3bdd25f4192a798267848fb7124f09000b4ba2
Josely Silva <joselysilva.dev@gmail.com>
2026-06-24 10:21:38 -0300

    Primeira versão da calculadora GPA

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string studentName = "Josely Silva";
+
+Console.WriteLine("=================================");
+Console.WriteLine("      GPA CALCULATOR");
+Console.WriteLine("=================================");
+Console.WriteLine();
+
+Console.WriteLine($"Student: {studentName}");
+Console.WriteLine("Final GPA: 3.59");
